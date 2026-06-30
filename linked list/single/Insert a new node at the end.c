e78660582f7137f389dc7661f56b89fc1b23b2a2
Dhinagaran M <dhinagaran357@gmail.com>
2026-06-30 15:45:38 +0530

    Add files via upload

---
+++ linked list/single/Insert a new node at the end.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+struct node
+{
+    char *data;
+    struct node *nextNode;
+}*head=NULL;
+void append(char *name)
+{
+    //1. create memory for the node
+    struct node *newnode = (struct node*)malloc(sizeof(struct node));
+    //2. initialize the values to the node memory
+    newnode->data = name;
+    newnode->nextNode = NULL;
+    //3. connection between them
+    if(head==NULL)
+    {
+        head = newnode;
+    }
+    else
+    {
+        struct node *lastNode = head;
+        while(lastNode->nextNode!=NULL)
+        {
+            lastNode = lastNode->nextNode;
+        }
+        lastNode->nextNode = newnode;
+    }
+}
+void display()
+{
+    struct node *temp = head;
+    while(temp->nextNode!=NULL)
+    {
+        printf("%s ",temp->data);
+        temp = temp->nextNode;
+    }
+    printf("%s\n",temp->data);
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        char *name = (char*)malloc(100+1);
+        scanf("%s",name);// c cpp java
+        append(name);
+    }
+    display();
+    return 0;
+}
---
+++ linked list/single/Insert a new node at the given position.c
@@ -0,0 +1,86 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+struct node
+{
+    char *data;
+    struct node *nextNode;
+}*head=NULL;
+void appendd(char *name,int position)
+{
+    //1. create memory for the node
+    struct node *newnode = (struct node*)malloc(sizeof(struct node));
+    //2. initialize the values to the node memory
+    newnode->data = name;
+    newnode->nextNode = NULL;
+    //3. connection between them
+    if(head==NULL)
+    {
+        head = newnode;
+    }
+    else
+    {  int count=1;
+    
+        struct node *lastnode=head;
+        while(count!=position){
+            lastnode=lastnode->nextNode;
+            count++;
+        }
+        struct node *temp=lastnode->nextNode;
+        lastnode->nextNode=newnode;
+        newnode->nextNode=temp;
+        
+    }
+}
+void append(char *name)
+{
+    //1. create memory for the node
+    struct node *newnode = (struct node*)malloc(sizeof(struct node));
+    //2. initialize the values to the node memory
+    newnode->data = name;
+    newnode->nextNode = NULL;
+    //3. connection between them
+    if(head==NULL)
+    {
+        head = newnode;
+    }
+    else
+    {
+        struct node *lastnode=head;
+        while(lastnode->nextNode!=NULL){
+            lastnode=lastnode->nextNode;
+            
+        }
+        lastnode->nextNode=newnode;
+        
+    }
+}
+void display()
+{
+    struct node *temp = head;
+    while(temp->nextNode!=NULL)
+    {
+        printf("%s ",temp->data);
+        temp = temp->nextNode;
+    }
+    printf("%s\n",temp->data);
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        char *name = (char*)malloc(100+1);
+        scanf("%s",name);// c cpp java
+        append(name);
+    }
+    display();
+    int position;
+    char *name = (char*)malloc(100+1);
+    scanf("%s data ",name);// c cpp java
+    scanf("%d position ",&position);
+     appendd(name,position);
+    display();
+    return 0;
+}
